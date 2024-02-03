9bc7b143113a61608c739c234d931d1a50b46ebb
Ziedelth <watteaupierre@gmail.com>
2024-02-03 20:37:45 +0100

    Fix queries

--- src/main/kotlin/fr/shikkanime/repositories/MemberRepository.kt
+++ src/main/kotlin/fr/shikkanime/repositories/MemberRepository.kt
@@ -5,16 +5,20 @@ import fr.shikkanime.entities.enums.Role
 
 class MemberRepository : AbstractRepository<Member>() {
     fun findAllByRole(role: Role): List<Member> {
-        return createQuery("FROM Member WHERE role = :role", getEntityClass())
-            .setParameter("role", role)
-            .resultList
+        return inTransaction {
+            createReadOnlyQuery(it, "FROM Member WHERE role = :role", getEntityClass())
+                .setParameter("role", role)
+                .resultList
+        }
     }
 
     fun findByUsernameAndPassword(username: String, password: ByteArray): Member? {
-        return createQuery("FROM Member WHERE username = :username AND encryptedPassword = :password", getEntityClass())
-            .setParameter("username", username)
-            .setParameter("password", password)
-            .resultList
-            .firstOrNull()
+        return inTransaction {
+            createReadOnlyQuery(it, "FROM Member WHERE username = :username AND encryptedPassword = :password", getEntityClass())
+                .setParameter("username", username)
+                .setParameter("password", password)
+                .resultList
+                .firstOrNull()
+        }
     }
 }
