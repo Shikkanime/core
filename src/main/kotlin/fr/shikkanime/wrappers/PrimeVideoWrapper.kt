ac6379234164c0a3f63dfaa1dcb639b054a28cf7
Ziedelth <watteaupierre@gmail.com>
2024-03-04 20:10:46 +0100

    Refresh prime video content

---
+++ src/main/kotlin/fr/shikkanime/wrappers/PrimeVideoWrapper.kt
@@ -0,0 +1,53 @@
+package fr.shikkanime.wrappers
+
+import com.google.gson.JsonObject
+import fr.shikkanime.entities.enums.LangType
+import fr.shikkanime.entities.enums.Platform
+import fr.shikkanime.utils.EncryptionManager
+import fr.shikkanime.utils.HttpRequest
+
+object PrimeVideoWrapper {
+    private val platform = Platform.PRIM
+
+    fun getShowVideos(countryCode: String, locale: String, id: String): List<JsonObject> {
+        val document =
+            HttpRequest().use { it.getBrowser("https://www.primevideo.com/-/${countryCode.lowercase()}/detail/$id?language=$locale") }
+        val showName = document.selectFirst("h1[data-automation-id=\"title\"]")!!.text()
+        val showBanner = document.selectFirst("img[data-testid=\"base-image\"]")!!.attr("src")
+        val showDescription = document.selectFirst(".dv-dp-node-synopsis")!!.text()
+        val domEpisodes = document.selectFirst("ol.GG33WY")!!.select("li.c5qQpO")
+
+        return domEpisodes.mapNotNull { domEpisode ->
+            val episodeTitle = domEpisode.selectFirst("span.P1uAb6")!!.text()
+            val episodeSeasonAndNumber = domEpisode.selectFirst("span.izvPPq > span")!!.text()
+            val season = episodeSeasonAndNumber.substringAfter("S. ").substringBefore(" ").trim().toIntOrNull() ?: 1
+            val episodeNumber =
+                episodeSeasonAndNumber.substringAfter("ÉP. ").substringBefore(" ").trim().toIntOrNull() ?: -1
+            val duration = domEpisode.selectFirst("div[data-testid=\"episode-runtime\"]")!!.text()
+            val durationInSeconds = duration.substringBefore("min").trim().toLongOrNull()?.times(60) ?: -1
+            val image = domEpisode.selectFirst("img[data-testid=\"base-image\"]")!!.attr("src")
+            val episodeDescription = domEpisode.selectFirst("div[dir=\"auto\"]")!!.text()
+
+            JsonObject().apply {
+                add("show", JsonObject().apply {
+                    addProperty("name", showName)
+                    addProperty("banner", showBanner)
+                    addProperty("description", showDescription)
+                })
+                addProperty(
+                    "id",
+                    "${countryCode}-$platform-${
+                        EncryptionManager.toSHA512("$id-${season}-$episodeNumber").substring(0..<8)
+                    }-${LangType.SUBTITLES}"
+                )
+                addProperty("season", season)
+                addProperty("number", episodeNumber)
+                addProperty("title", episodeTitle)
+                addProperty("url", "https://www.primevideo.com/-/${countryCode.lowercase()}/detail/$id")
+                addProperty("image", image)
+                addProperty("duration", durationInSeconds)
+                addProperty("description", episodeDescription)
+            }
+        }
+    }
+}
