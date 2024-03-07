2d6e252a63a20037b634b1386894e57df99462ad
Ziedelth <watteaupierre@gmail.com>
2024-03-07 11:07:33 +0100

    Remove unused kotlinVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import java.net.URI
 
 val ktorVersion = "2.3.9"
-val kotlinVersion = "1.9.22"
 val ktorSwaggerUiVersion = "2.7.4"
 val hibernateCoreVersion = "6.4.4.Final"
 val ehcacheVersion = "3.10.8"
