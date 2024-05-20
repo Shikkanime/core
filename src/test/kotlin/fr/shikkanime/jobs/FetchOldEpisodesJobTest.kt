9896961cbd75d36f33e4237c37dcf26d918dfd7e
Ziedelth <watteaupierre@gmail.com>
2024-05-20 10:41:32 +0200

    Add test on FetchOldEpisodesJob.kt

---
+++ src/test/kotlin/fr/shikkanime/jobs/FetchOldEpisodesJobTest.kt
@@ -0,0 +1,29 @@
+package fr.shikkanime.jobs
+
+import com.google.inject.Inject
+import fr.shikkanime.caches.CountryCodeIdKeyCache
+import fr.shikkanime.entities.enums.CountryCode
+import fr.shikkanime.utils.Constant
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class FetchOldEpisodesJobTest {
+    @Inject
+    private lateinit var fetchOldEpisodesJob: FetchOldEpisodesJob
+
+    @BeforeEach
+    fun setUp() {
+        Constant.injector.injectMembers(this)
+    }
+
+    @Test
+    fun `check if Atelier Riza is correctly fetch`() {
+        val episodes = fetchOldEpisodesJob.crunchyrollEpisodesCache[CountryCodeIdKeyCache(CountryCode.FR, "GEXH3W2Z5")]
+
+        // If episodes contains the episode 1, it means that the job has worked
+        assertNotNull(episodes)
+        assertTrue(episodes!!.any { it.seasonNumber == 1 && it.number == 1 })
+    }
+}
