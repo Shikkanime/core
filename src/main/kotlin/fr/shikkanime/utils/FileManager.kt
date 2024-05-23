589c6333454dcabff3aba4e855496a023d12671d
Ziedelth <watteaupierre@gmail.com>
2024-05-23 13:50:22 +0200

    Optimize ImageService.kt on load and save

--- src/main/kotlin/fr/shikkanime/utils/FileManager.kt
+++ src/main/kotlin/fr/shikkanime/utils/FileManager.kt
@@ -5,32 +5,10 @@ import org.opencv.core.MatOfByte
 import org.opencv.core.MatOfInt
 import org.opencv.imgcodecs.Imgcodecs
 import java.io.BufferedInputStream
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.util.zip.GZIPInputStream
-import java.util.zip.GZIPOutputStream
-import java.util.zip.ZipEntry
-import java.util.zip.ZipOutputStream
 
 object FileManager {
     init {
         OpenCV.loadLocally()
-    }
-
-    fun toGzip(bytes: ByteArray): ByteArray {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        val gzip = GZIPOutputStream(byteArrayOutputStream)
-        gzip.write(bytes)
-        gzip.close()
-        return byteArrayOutputStream.toByteArray()
-    }
-
-    fun fromGzip(bytes: ByteArray): ByteArray {
-        val gzip = GZIPInputStream(ByteArrayInputStream(bytes))
-        val compressed = gzip.readBytes()
-        gzip.close()
-        return compressed
     }
 
     fun encodeToWebP(image: ByteArray): ByteArray {
@@ -43,19 +21,6 @@ object FileManager {
         } else {
             throw Exception("Failed to encode image to WebP")
         }
-    }
-
-    fun zipFiles(zipFile: File, list: List<Pair<String, ByteArray>>) {
-        val zipOutputStream = ZipOutputStream(zipFile.outputStream())
-
-        list.forEach { file ->
-            val zipEntry = ZipEntry(file.first)
-            zipOutputStream.putNextEntry(zipEntry)
-            zipOutputStream.write(file.second)
-            zipOutputStream.closeEntry()
-        }
-
-        zipOutputStream.close()
     }
 
     fun getInputStreamFromResource(resource: String): BufferedInputStream {
