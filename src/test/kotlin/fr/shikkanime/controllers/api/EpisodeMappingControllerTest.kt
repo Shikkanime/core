3c1e9712fbb75b4923a32b569831d810ee8b1520
Ziedelth <watteaupierre@gmail.com>
2024-07-23 12:12:07 +0200

    Improve tests coverage

---
+++ src/test/kotlin/fr/shikkanime/controllers/api/EpisodeMappingControllerTest.kt
@@ -0,0 +1,32 @@
+package fr.shikkanime.controllers.api
+
+import fr.shikkanime.dtos.PageableDto
+import fr.shikkanime.module
+import fr.shikkanime.utils.ObjectParser
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class EpisodeMappingControllerTest : AbstractControllerTest() {
+    @Test
+    fun getAll() {
+        testApplication {
+            application {
+                module()
+            }
+
+            client.get("/api/v1/episode-mappings?sort=lastReleaseDateTime,animeName,season,episodeType,number&desc=lastReleaseDateTime,animeName,season,episodeType,number&page=1&limit=4") {
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            }.apply {
+                assertEquals(HttpStatusCode.OK, status)
+                val episodeMappingsDto = ObjectParser.fromJson(bodyAsText(), PageableDto::class.java)
+                assertEquals(4, episodeMappingsDto.data.size)
+                assertTrue(episodeMappingsDto.total > 4)
+            }
+        }
+    }
+}
