23cd1d8c8327da0f0f33b81dbb2b1ca1f792008c
Ziedelth <watteaupierre@gmail.com>
2024-09-26 17:43:22 +0200

    fix: current simulcast not used in cache when it's already exists

--- src/main/kotlin/fr/shikkanime/services/caches/SimulcastCacheService.kt
+++ src/main/kotlin/fr/shikkanime/services/caches/SimulcastCacheService.kt
@@ -3,6 +3,7 @@ package fr.shikkanime.services.caches
 import com.google.inject.Inject
 import fr.shikkanime.converters.AbstractConverter
 import fr.shikkanime.dtos.SimulcastDto
+import fr.shikkanime.entities.Anime
 import fr.shikkanime.entities.Simulcast
 import fr.shikkanime.services.SimulcastService
 import fr.shikkanime.utils.MapCache
@@ -13,11 +14,11 @@ class SimulcastCacheService : AbstractCacheService {
     @Inject
     private lateinit var simulcastService: SimulcastService
 
-    private val cache = MapCache<String, List<SimulcastDto>>(classes = listOf(Simulcast::class.java)) {
+    private val cache = MapCache<String, List<SimulcastDto>>(classes = listOf(Simulcast::class.java, Anime::class.java)) {
         AbstractConverter.convert(simulcastService.findAll(), SimulcastDto::class.java)!!
     }
 
-    private val modifiedCache = MapCache<String, List<SimulcastDto>>(classes = listOf(Simulcast::class.java)) {
+    private val modifiedCache = MapCache<String, List<SimulcastDto>>(classes = listOf(Simulcast::class.java, Anime::class.java)) {
         val list = simulcastService.findAllModified().map { it[0] as Simulcast to it[1] as ZonedDateTime }
 
         AbstractConverter.convert(list.map { it.first }, SimulcastDto::class.java)!!.onEach { simulcastDto ->
