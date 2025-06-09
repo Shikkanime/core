c253286e1889c5fe532ddfd155c4850f4c3938fe
Ziedelth <watteaupierre@gmail.com>
2025-06-09 11:39:10 +0200

    Add last validation date time to anime platform details

--- src/main/kotlin/fr/shikkanime/dtos/AnimePlatformDto.kt
+++ src/main/kotlin/fr/shikkanime/dtos/AnimePlatformDto.kt
@@ -1,9 +1,10 @@
 package fr.shikkanime.dtos
 
-import java.util.UUID
+import java.util.*
 
 data class AnimePlatformDto(
     val uuid: UUID?,
     val platform: PlatformDto,
     val platformId: String,
+    val lastValidateDateTime: String?,
 )
