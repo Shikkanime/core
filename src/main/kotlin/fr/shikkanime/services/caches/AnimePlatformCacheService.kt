daff1fb6544481dc908dbfdd8ec122253d3dea3c
Ziedelth <watteaupierre@gmail.com>
2025-10-02 11:08:23 +0200

    Revise VF detection in AnimationDigitalNetworkPlatform to handle audio locale delays and improve video retrieval logic

--- src/main/kotlin/fr/shikkanime/services/caches/AnimePlatformCacheService.kt
+++ src/main/kotlin/fr/shikkanime/services/caches/AnimePlatformCacheService.kt
@@ -4,11 +4,13 @@ import com.google.gson.reflect.TypeToken
 import com.google.inject.Inject
 import fr.shikkanime.dtos.AnimePlatformDto
 import fr.shikkanime.entities.Anime
+import fr.shikkanime.entities.enums.Platform
 import fr.shikkanime.factories.impl.AnimePlatformFactory
 import fr.shikkanime.services.AnimePlatformService
 import fr.shikkanime.utils.MapCache
 import fr.shikkanime.utils.MapCacheValue
 import fr.shikkanime.utils.SerializationUtils
+import java.util.*
 
 class AnimePlatformCacheService : ICacheService {
     @Inject private lateinit var animePlatformService: AnimePlatformService
@@ -21,4 +23,11 @@ class AnimePlatformCacheService : ICacheService {
         serializationType = SerializationUtils.SerializationType.JSON,
         key = anime.uuid!!,
     ) { uuid -> animePlatformService.findAllByAnime(uuid).map { animePlatformFactory.toDto(it) }.toTypedArray() }
+
+    fun findAllIdByAnimeAndPlatform(animeUuid: UUID, platform: Platform) = MapCache.getOrCompute(
+        "AnimePlatformCacheService.findAllIdByAnimeAndPlatform",
+        classes = listOf(UUID::class.java, Platform::class.java),
+        typeToken = object : TypeToken<MapCacheValue<Array<String>>>() {},
+        key = animeUuid to platform,
+    ) { (animeUuid, platform) -> animePlatformService.findAllIdByAnimeAndPlatform(animeUuid, platform).toTypedArray() }
 }
