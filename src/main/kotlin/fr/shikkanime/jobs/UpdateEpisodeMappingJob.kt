98790c8e1771cc6af59904d726cf0ee541507b80
Ziedelth <watteaupierre@gmail.com>
2025-12-04 17:36:51 +0100

    Clean up code

--- src/main/kotlin/fr/shikkanime/jobs/UpdateEpisodeMappingJob.kt
+++ src/main/kotlin/fr/shikkanime/jobs/UpdateEpisodeMappingJob.kt
@@ -466,12 +466,10 @@ class UpdateEpisodeMappingJob : AbstractJob {
             updateIdentifier(episodeVariant, id, episode.id.toString(), identifiers)
             updateUrl(episodeVariant, episode.url)
 
-            episodes.add(
+            episodes.addAll(
                 netflixPlatform.convertEpisode(
                     countryCode,
-                    StringUtils.EMPTY_STRING,
                     episode,
-                    episodeVariant.audioLocale!!
                 )
             )
         }
@@ -570,7 +568,7 @@ class UpdateEpisodeMappingJob : AbstractJob {
 
                 // Fetch episodes by series and find the previous episode
                 logger.warning("Previous episode not found in season, searching by series...")
-                return runCatching { CrunchyrollCachedWrapper.getEpisodesBySeriesId(countryCode.locale, episode.seriesId) }.getOrNull()
+                runCatching { CrunchyrollCachedWrapper.getEpisodesBySeriesId(countryCode.locale, episode.seriesId) }.getOrNull()
                     ?.sortedWith(compareBy({ it.episodeMetadata!!.seasonSequenceNumber }, { it.episodeMetadata!!.sequenceNumber }))
                     ?.lastOrNull { it.episodeMetadata!!.index() < episode.index() }
                     ?.id
@@ -619,7 +617,7 @@ class UpdateEpisodeMappingJob : AbstractJob {
 
                 // Fetch episodes by series and find the next episode
                 logger.warning("Next episode not found in season, searching by series...")
-                return runCatching { CrunchyrollCachedWrapper.getEpisodesBySeriesId(countryCode.locale, episode.seriesId) }.getOrNull()
+                runCatching { CrunchyrollCachedWrapper.getEpisodesBySeriesId(countryCode.locale, episode.seriesId) }.getOrNull()
                     ?.sortedWith(compareBy({ it.episodeMetadata!!.seasonSequenceNumber }, { it.episodeMetadata!!.sequenceNumber }))
                     ?.firstOrNull { it.episodeMetadata!!.index() > episode.index() }
                     ?.id
--- src/main/kotlin/fr/shikkanime/platforms/configuration/PrimeVideoConfiguration.kt
+++ src/main/kotlin/fr/shikkanime/platforms/configuration/PrimeVideoConfiguration.kt
@@ -2,15 +2,30 @@ package fr.shikkanime.platforms.configuration
 
 import fr.shikkanime.utils.StringUtils
 import io.ktor.http.*
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class PrimeVideoConfiguration : PlatformConfiguration<PrimeVideoConfiguration.PrimeVideoSimulcast>() {
-    data class PrimeVideoSimulcast(var releaseTime: String = StringUtils.EMPTY_STRING) : ReleaseDayPlatformSimulcast() {
+    data class PrimeVideoSimulcast(
+        var image: String = StringUtils.EMPTY_STRING,
+        var releaseTime: String = StringUtils.EMPTY_STRING
+    ) : ReleaseDayPlatformSimulcast() {
         override fun of(parameters: Parameters) {
             super.of(parameters)
+            parameters["image"]?.let { image = it }
             parameters["releaseTime"]?.let { releaseTime = it }
         }
 
         override fun toConfigurationFields() = super.toConfigurationFields().apply {
+            add(
+                ConfigurationField(
+                    label = "Image",
+                    name = "image",
+                    type = "text",
+                    value = image
+                ),
+            )
             add(
                 ConfigurationField(
                     label = "Release time",
@@ -27,6 +42,7 @@ class PrimeVideoConfiguration : PlatformConfiguration<PrimeVideoConfiguration.Pr
             if (other !is PrimeVideoSimulcast) return false
             if (!super.equals(other)) return false
 
+            if (image != other.image) return false
             if (releaseTime != other.releaseTime) return false
 
             return true
@@ -34,8 +50,20 @@ class PrimeVideoConfiguration : PlatformConfiguration<PrimeVideoConfiguration.Pr
 
         override fun hashCode(): Int {
             var result = super.hashCode()
+            result = 31 * result + image.hashCode()
             result = 31 * result + releaseTime.hashCode()
             return result
+        }
+
+        override fun canBeFetch(zonedDateTime: ZonedDateTime): Boolean {
+            if (!super.canBeFetch(zonedDateTime)) return false
+            if (this.releaseTime.isBlank()) return true
+
+            val releaseLocalTime = LocalTime.parse(this.releaseTime)
+            val isAfterInOriginalZone = zonedDateTime.toLocalTime() >= releaseLocalTime
+            val isAfterInSystemZone = zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toLocalTime() >= releaseLocalTime
+
+            return isAfterInOriginalZone || isAfterInSystemZone
         }
     }
 
