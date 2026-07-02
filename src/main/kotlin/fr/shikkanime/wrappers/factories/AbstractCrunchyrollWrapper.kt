faae47cecf44e30a4278d696e8553b1d08ae3394
Ziedelth <45169338+Ziedelth@users.noreply.github.com>
2026-07-02 16:01:16 +0000

    [update-credentials action] Update Crunchyroll basic auth token

--- src/main/kotlin/fr/shikkanime/wrappers/factories/AbstractCrunchyrollWrapper.kt
+++ src/main/kotlin/fr/shikkanime/wrappers/factories/AbstractCrunchyrollWrapper.kt
@@ -362,7 +362,7 @@ abstract class AbstractCrunchyrollWrapper {
     companion object {
         const val CRUNCHYROLL_CHUNK = 100
         // Updated automatically by the update-credentials GitHub Actions workflow - do not edit manually
-        const val CRUNCHYROLL_BASIC_AUTH_TOKEN_DEFAULT = "bGFzcnF6eGJlbXZvcWlveTU2bTA6ZHlodDVSWVYyXzIyUm4xaWF0X29YV0c2ejBUWUswazE="
-        const val CRUNCHYROLL_APK_VERSION = "3.66.0_22348"
+        const val CRUNCHYROLL_BASIC_AUTH_TOKEN_DEFAULT = "bWMyNDBjZTc1dzduYmRoYmIwcTY6RWN5c2JiV2RYbzFSMFdXQ1RFNTVEUGxabndXNC1PNWI="
+        const val CRUNCHYROLL_APK_VERSION = "3.66.1_22349"
     }
 }
